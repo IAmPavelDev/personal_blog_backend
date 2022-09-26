32bdb7881a5aad753fda2c182e1cd40561906dc0
IAmPavelDev <179werpavel@protonmail.com>
2022-09-27 02:04:05 +0300

    build

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
